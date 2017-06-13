8bd0abe0d7461feef4c447a9d779afa9bbb2569e
Nick Redshaw <nickredshaw@gmail.com>
2017-06-13 17:50:16 +0100

    Convert sound files to mp3

--- sound-fx.js
+++ sound-fx.js
@@ -1,9 +1,9 @@
 var sounds = {
-    shipMissile: new Audio('res/ship-missile.wav'),
-    saucerMissile: new Audio('res/saucer-missile.wav'),
-    explosion1: new Audio('res/explosion1.wav'),
-    explosion2: new Audio('res/explosion2.wav'),
-    explosion3: new Audio('res/explosion3.wav')
+    shipMissile: new Audio('res/ship-missile.mp3'),
+    saucerMissile: new Audio('res/saucer-missile.mp3'),
+    explosion1: new Audio('res/explosion1.mp3'),
+    explosion2: new Audio('res/explosion2.mp3'),
+    explosion3: new Audio('res/explosion3.mp3')
 };
 
 function playSound(name) {
